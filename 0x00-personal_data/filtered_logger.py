1890cda6c6f3b56f161e8e3e6d21fe6cb17a4d8e
Ada <106437177+Coding-doves@users.noreply.github.com>
2024-02-07 15:42:28 +0100

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
 ''' filter_datum '''
 import re
+import logging
 
 
 def filter_datum(fields, redaction, message, separator):
     return re.sub(r'(?<=^|\{0})[^{1}]+(?={0}|$)'.format(separator, separator), redaction, message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
