c8bb68c381ea4b190d7da00da2a8bc8565175995
Ada <106437177+Coding-doves@users.noreply.github.com>
2024-02-07 15:56:37 +0100

    Update filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,9 @@
 ''' filter_datum '''
 import re
 import logging
+from logging import StreamHandler
+from filtered_formatter import RedactingFormatter
+import csv
 
 
 def filter_datum(fields, redaction, message, separator):
@@ -23,3 +26,20 @@ class RedactingFormatter(logging.Formatter):
     def format(self, record: logging.LogRecord) -> str:
         record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
         return super().format(record)
+
+
+PII_FIELDS = ("name", "email", "phone", "address", "credit_card")
+
+def get_logger():
+    ''' logger '''
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    formatter = RedactingFormatter(PII_FIELDS)
+    stream_handler = StreamHandler()
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+
+    return logger
