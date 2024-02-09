4e7794f6126650e38e35a1a307d54dbfd552e623
dove <obenedicta4@gmail.com>
2024-02-09 12:24:53 +0100

    update

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,7 +6,7 @@ hash password
 import bcrypt
 
 
-def hash_password(password):
+def hash_password(password: str) -> bytes:
     ''' Generate a salt and hash the password '''
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
@@ -14,6 +14,6 @@ def hash_password(password):
     return hashed_password
 
 
-def is_valid(hashed_password, password):
+def is_valid(hashed_password: bytes, password: str) -> bool:
     ''' Check if the password matches the hashed password '''
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
