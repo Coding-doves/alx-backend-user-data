d280ccf08845bea7a3d243e1fd649c6289ccfa4f
dove <obenedicta4@gmail.com>
2024-02-09 12:26:46 +0100

    update

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,7 +10,7 @@ def hash_password(password: str) -> bytes:
     ''' Generate a salt and hash the password '''
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
-    
+
     return hashed_password
 
 
