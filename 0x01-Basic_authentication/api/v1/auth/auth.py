c16f7639f023654bbc70f209cc72552901810356
dove <obenedicta4@gmail.com>
2024-02-14 14:30:11 +0100

    update

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -23,11 +23,11 @@ class Auth:
         ''' authorization_header '''
         if request is None:
             return None
-        
+
         header = request.headers.get("Authorization")
         if header is None:
             return None
-        
+
         return header
 
     def current_user(self, request=None) -> TypeVar('User'):
