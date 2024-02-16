1f82b5959e021bfb534a0801a517b93b7d5fd77b
Ada <106437177+Coding-doves@users.noreply.github.com>
2024-02-16 22:27:42 +0100

    Update basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -71,8 +71,8 @@ class BasicAuth(Auth):
                 return None
             if not user:
                 return None
-            if len(user) <= 0:
-                return None
+            # if len(user) <= 0:
+                # return None
             if user[0].is_valid_password(user_pwd):
                 return user[0]
         return None
