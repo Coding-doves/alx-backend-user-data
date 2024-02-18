d21d4cb29256294b8f160a926600f1439cbe489c
Ada <106437177+Coding-doves@users.noreply.github.com>
2024-02-18 21:56:14 +0100

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -33,7 +33,7 @@ def user_login() -> Tuple[str, int]:
         session_id = auth.create_session(getattr(user[0], "id"))
         response = jsonify(user[0].to_json())
         response.set_cookie(getenv("SESSION_NAME"), session_id)
-        return response
+        return response, 200
     return jsonify({ "error": "wrong password" }), 401
 
 @app_views.route(
@@ -44,8 +44,7 @@ def user_login() -> Tuple[str, int]:
 def logout() -> Tuple[str, int]:
     ''' logout '''
     from api.v1.app import auth
-    ''' logout '''
     destroy_session = auth.destroy_session(request)
     if not destroy_session:
         abort(404)
-    return jsonify({})
+    return jsonify({}), 200
