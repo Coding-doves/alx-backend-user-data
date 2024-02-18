62330996f03d2bcfc0ca6ea10984470b932faabf
Ada <106437177+Coding-doves@users.noreply.github.com>
2024-02-18 23:13:41 +0100

    Update session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -34,7 +34,7 @@ class SessionAuth(Auth):
         user_id = self.user_id_for_session_id(self.session_cookie(request))
         return User.get(user_id)
 
-    def destroy_session(self, request=None):
+    def destroy_session(self, request=None) -> bool:
         ''' logout and destory '''
         session_id = self.session_cookie(request)
         user = self.user_id_by_session_id(session_id)
