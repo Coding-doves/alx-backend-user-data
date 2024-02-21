17303db151d33a699713f093b38ef1806fb58c09
dove <obenedicta4@gmail.com>
2024-02-21 22:09:53 +0100

    update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,4 +3,28 @@
 from sqlalchemy.orm.exc import NoResultFound
 from db import DB
 import bcrypt
-from uuid import
+from uuid import uuid4
+from user import User
+from typing import Union
+
+
+def _hash_password(password: str) -> bytes:
+    ''' hashed paswd '''
+    pwd = password.encode('utf-8')
+    return bcrypt.hashpw(pwd, bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        ''' register user '''
+        user = self._db.find_user_by(email=email)
+        hashed_password = hashed_password(password)
+        if user is not None:
+            raise ValueError(f"User {email} already exists")
+        return self._db.add_user(email, hashed_password)
--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -3,14 +3,40 @@
 Main file
 """
 
-from db import DB
-from user import User
 
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
+from auth import Auth
+
+email = 'me@me.com'
+password = 'mySecuredPwd'
+
+auth = Auth()
+
+try:
+    user = auth.register_user(email, password)
+    print("successfully created a new user!")
+except ValueError as err:
+    print("could not create a new user: {}".format(err))
+
+try:
+    user = auth.register_user(email, password)
+    print("successfully created a new user!")
+except ValueError as err:
+    print("could not create a new user: {}".format(err))  
 
 
-my_db = DB()
+# from auth import _hash_password
+
+# print(_hash_password("Hello Holberton"))
+
+
+# from db import DB
+# from user import User
+
+# from sqlalchemy.exc import InvalidRequestError
+# from sqlalchemy.orm.exc import NoResultFound
+
+
+# my_db = DB()
 
 # email = 'test@test.com'
 # hashed_password = "hashedPwd"
@@ -48,8 +74,8 @@ my_db = DB()
 
 # my_db = DB()
 
-user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
-print(user_1.id)
+# user_1 = my_db.add_user("test@test.com", "SuperHashedPwd")
+# print(user_1.id)
 
-user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
-print(user_2.id)
+# user_2 = my_db.add_user("test1@test.com", "SuperHashedPwd1")
+# print(user_2.id)
