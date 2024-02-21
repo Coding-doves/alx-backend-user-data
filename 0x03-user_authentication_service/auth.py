0850f350c962c9c28f6e2f5e9bfc2c1348af075c
dove <obenedicta4@gmail.com>
2024-02-21 22:34:39 +0100

    update

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -24,7 +24,7 @@ class Auth:
     def register_user(self, email: str, password: str) -> User:
         ''' register user '''
         user = self._db.find_user_by(email=email)
-        hashed_password = hashed_password(password)
         if user is not None:
             raise ValueError(f"User {email} already exists")
+        hashed_password = _hashed_password(password)
         return self._db.add_user(email, hashed_password)
